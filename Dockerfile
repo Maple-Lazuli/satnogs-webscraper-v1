f78435f8ffe9d48e816edbf3d1d02707eb0720d7
Maple-Lazuli <the.nostra.tymus@gmail.com>
2022-05-13 04:43:37 -0400

    Dockerized the webscraper

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM ubuntu
+
+COPY src /opt/app/src
+
+COPY requirements.txt /opt/app/
+COPY keys.txt /opt/app/
+
+RUN apt update
+
+RUN apt upgrade -y
+
+RUN apt install python3 -y
+
+RUN apt install python3-pip -y
+
+RUN apt install python3.10-venv -y
+
+RUN pip install -r opt/app/requirements.txt
+
+RUN mkdir /opt/app/data
+
+WORKDIR /opt/app
+
+CMD [ "python3", "-m" , "src.data_pull"]
--- src/data_pull.py
+++ src/data_pull.py
@@ -57,9 +57,12 @@ def prepare_directory():
         cnst.directories['logs'],
     ]
 
-    if os.path.exists(root_dir):
+    # Second part is a quick fix for a docker issue
+    os.makedirs(root_dir, exist_ok=True)
+
+    if len([file for file in os.listdir(root_dir)]) > 0:
         shutil.rmtree(root_dir)
-    os.makedirs(root_dir)
+        os.makedirs(root_dir, exist_ok=True)
 
     for d in sub_dirs:
         os.makedirs(d, exist_ok=True)
@@ -68,19 +71,19 @@ def prepare_directory():
 if __name__ == '__main__':
     # Pull list of satellite IDs from SATNOGs Database
     prepare_directory()
-    # sat = Satellites()
-    # sat_ids = sat.get_dataframe().index.values
-    # # Use satellite IDs to query TM events and find observation IDs
-    # tm = Telemetry(prints=True, max_pages=1)
-    # tm.clear_archived_events()
-    # tm.multiprocess_fetch(sat_ids, update_tm_events=True)
-    # tm_df = tm.get_events_df(save_csv=True)
-    # # extract observation IDs from the telemetry data frame
-    # tm_df['observation_id'] = tm_df['observation_id'].fillna(0)
-    # tm_df['observation_id'] = tm_df['observation_id'].astype(int)
-    # observations = pd.unique(tm_df[tm_df['observation_id'] > 0]['observation_id'])
-    # # start web scraping from the observation IDs
-    # scraper = ObservationScraper()
-    # scraper.multiprocess_scrape_observations(observations)
-    # obs_df = scraper.get_dataframe(save_csv=True)
-    # complete_dataset()
+    sat = Satellites()
+    sat_ids = sat.get_dataframe().index.values
+    # Use satellite IDs to query TM events and find observation IDs
+    tm = Telemetry(prints=True)
+    tm.clear_archived_events()
+    tm.multiprocess_fetch(sat_ids, update_tm_events=True)
+    tm_df = tm.get_events_df(save_csv=True)
+    # extract observation IDs from the telemetry data frame
+    tm_df['observation_id'] = tm_df['observation_id'].fillna(0)
+    tm_df['observation_id'] = tm_df['observation_id'].astype(int)
+    observations = pd.unique(tm_df[tm_df['observation_id'] > 0]['observation_id'])
+    # start web scraping from the observation IDs
+    scraper = ObservationScraper()
+    scraper.multiprocess_scrape_observations(observations)
+    obs_df = scraper.get_dataframe(save_csv=True)
+    complete_dataset()
